a5e17345e1c701986ff3e94b6bdbb5fb5a971c88
Stepan Omelyanenko <ose.26@mail.ru>
2022-03-11 12:34:20 +0500

    Реализована функция RenderElement

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {renderElement, RenderPosition} from './render.js';
+import {render, RenderPosition} from './render.js';
 import TripTabsView from './view/trip-tabs-view.js';
 import TripFiltersView from './view/trip-filters-view.js';
 import TripSortView from './view/trip-sort-view.js';
@@ -17,13 +17,46 @@ const tripControlsFiltersElement = document.querySelector('.trip-controls__filte
 const tripEventsElement = document.querySelector('.trip-events');
 const tripEventsListElement = new EventsListView();
 
-renderElement(tripEventsElement, tripEventsListElement.element, RenderPosition.BEFOREEND);
-renderElement(tripControlsNavigationElement, new TripTabsView().element, RenderPosition.BEFOREEND);
-renderElement(tripControlsFiltersElement, new TripFiltersView().element, RenderPosition.BEFOREEND);
-renderElement(tripEventsElement, new TripSortView().element, RenderPosition.AFTERBEGIN);
-renderElement(tripEventsListElement.element, new EventItemAddView(tripEvents[1]).element, RenderPosition.BEFOREEND);
-renderElement(tripEventsListElement.element, new EventItemEditView(tripEvents[0]).element, RenderPosition.BEFOREEND);
+render(tripEventsElement, tripEventsListElement.element, RenderPosition.BEFOREEND);
+render(tripControlsNavigationElement, new TripTabsView().element, RenderPosition.BEFOREEND);
+render(tripControlsFiltersElement, new TripFiltersView().element, RenderPosition.BEFOREEND);
+render(tripEventsElement, new TripSortView().element, RenderPosition.AFTERBEGIN);
+render(tripEventsListElement.element, new EventItemAddView(tripEvents[1]).element, RenderPosition.BEFOREEND);
+//render(tripEventsListElement.element, new EventItemEditView(tripEvents[0]).element, RenderPosition.BEFOREEND);
 
-for (let i = 2; i < TRIP_EVENTS_COUNT; i++) {
-  renderElement(tripEventsListElement.element, new TripEventItemView(tripEvents[i]).element, RenderPosition.BEFOREEND);
+const renderEvent = (eventListElement, event) => {
+  const eventItemComponent = new TripEventItemView(event);
+  const eventEditComponent = new EventItemEditView(event);
+
+  const replaceItemToForm = () => {
+    eventListElement.replaceChild(eventEditComponent.element, eventItemComponent.element);
+  };
+  const replaceFormToItem = () => {
+    eventListElement.replaceChild(eventItemComponent.element, eventEditComponent.element);
+  };
+
+  const onEscKeyDown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      replaceFormToItem();
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
+  eventItemComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+    replaceItemToForm();
+    document.addEventListener('keydown', onEscKeyDown);
+  });
+
+  eventEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    replaceFormToItem();
+    document.removeEventListener('keydown', onEscKeyDown);
+  });
+
+  render(eventListElement, eventItemComponent.element, RenderPosition.BEFOREEND);
+};
+
+for (let i = 1; i < TRIP_EVENTS_COUNT; i++) {
+  renderEvent(tripEventsListElement.element, tripEvents[i]);
 }
