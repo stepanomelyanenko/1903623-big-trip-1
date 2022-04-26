2face01d80776c7b2090754d283144223ee6f722
Stepan Omelyanenko <ose.26@mail.ru>
2022-04-26 14:54:46 +0500

    6.1.5. Set Inner handlers

--- src/view/point-edit-view.js
+++ src/view/point-edit-view.js
@@ -52,10 +52,10 @@ const createPointEditTemplate = (point) => {
 
                   <div class="event__field-group  event__field-group--time">
                     <label class="visually-hidden" for="event-start-time-1">From</label>
-                    <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${DatetimeFrom}">
+                    <input class="event__input  event__input--time event__input-start-time" id="event-start-time-1" type="text" name="event-start-time" value="${DatetimeFrom}">
                     —
                     <label class="visually-hidden" for="event-end-time-1">To</label>
-                    <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${DatetimeTo}">
+                    <input class="event__input  event__input--time event__input-end-time" id="event-end-time-1" type="text" name="event-end-time" value="${DatetimeTo}">
                   </div>
 
                   <div class="event__field-group  event__field-group--price">
@@ -88,30 +88,78 @@ const createPointEditTemplate = (point) => {
             </li>`;
 };
 
-export default class PointEditView extends AbstractView {
-  #tripPoint = null;
+export default class PointEditView extends SmartView {
 
-  constructor(tripPoint) {
+  constructor(point) {
     super();
-    this.#tripPoint = tripPoint;
+    this._data = PointEditView.parsePointToData(point);
+
+    this.#setInnerHandlers();
   }
 
   get template() {
-    return createPointEditTemplate(this.#tripPoint);
+    return createPointEditTemplate(this._data);
   }
 
-
-  setFormSubmitHandler = (callback) => {
-    this._callback.formSubmit = callback;
-    this.element.querySelector('form').addEventListener('submit', this.#formSubmitHandler);
+  restoreHandlers = () => {
+    this.#setInnerHandlers();
+    this.setRollupClickHandler(this._callback.rollupClick);
+    this.setFormSubmitHandler(this._callback.formSubmit);
   }
 
-  #formSubmitHandler = (evt) => {
+  #setInnerHandlers = () => {
+    this.element.querySelector('.event__type-group')
+      .addEventListener('change', this.#typeGroupClickHandler);
+    this.element.querySelector('.event__input--destination')
+      .addEventListener('change', this.#destinationChangeHandler);
+    this.element.querySelector('.event__input-start-time')
+      .addEventListener('change', this.#startTimeChangeHandler);
+    this.element.querySelector('.event__input-end-time')
+      .addEventListener('change', this.#endTimeChangeHandler);
+    this.element.querySelector('.event__input--price')
+      .addEventListener('change', this.#basePriceChangeHandler);
+  }
+
+  #typeGroupClickHandler = (evt) => {
     evt.preventDefault();
-    this._callback.formSubmit();
-    this._callback.formSubmit(this.#tripPoint);
+    
+    this.updateData({
+      type: evt.target.value
+    }, false);
   }
 
+  #destinationChangeHandler = (evt) => {
+    evt.preventDefault();
+    this.updateData({
+      destination: this.#getChangedDestination(evt.target.value)
+    }, false);
+  }
+
+  #startTimeChangeHandler = (evt) => {
+    evt.preventDefault();
+    this.updateData({
+      date_from: evt.target.value //ИСПРАВИТЬ!!!!!!!!!!!!
+    }, true);
+    console.log(evt.target.value);
+  }
+
+  #endTimeChangeHandler = (evt) => {
+    evt.preventDefault();
+    this.updateData({
+      date_to: evt.target.value //ИСПРАВИТЬ!!!!!!!!!!!!
+    }, true);
+    console.log(evt.target.value);
+  }
+
+  #basePriceChangeHandler = (evt) => {
+    evt.preventDefault();
+    this.updateData({
+      base_price: evt.target.value
+    }, true);
+    console.log(evt.target.value);
+  }
+
+  //тут все хорошо
   setRollupClickHandler = (callback) => {
     this._callback.rollupClick = callback;
     this.element.querySelector('.event__rollup-btn').addEventListener('click', this.#rollupClickHandler);
@@ -121,6 +169,39 @@ export default class PointEditView extends AbstractView {
     evt.preventDefault();
     this._callback.rollupClick();
   }
+
+  setFormSubmitHandler = (callback) => {
+    this._callback.formSubmit = callback;
+    this.element.querySelector('form').addEventListener('submit', this.#formSubmitHandler);
+  }
+
+  #formSubmitHandler = (evt) => {
+    evt.preventDefault();
+    this._callback.formSubmit();
+    this._callback.formSubmit(this._data);
+  }
+
+  static parsePointToData = (task) => ({...task,
+    // isDueDate: task.dueDate !== null,
+    // isRepeating: isTaskRepeating(task.repeating),
+  });
+
+  //тут вспомогательные штуки
+  #getChangedDestination = (destinationName) => {
+    const allDestinations = destinations();
+
+    for (let i = 0; i < allDestinations.length; i++) {
+      if (allDestinations[i].name === destinationName) {
+        return allDestinations[i];
+      }
+    }
+
+    return {
+      'description': null,
+      'name': '',
+      'pictures': []
+    };
+  };
 }
 
 export class TaskEditView extends SmartView {
@@ -145,12 +226,6 @@ export class TaskEditView extends SmartView {
 
     //Слушатель для предложений.
 
-    const offers1 = this.element.querySelectorAll('.event__offer-selector');
-    for (let i = 0; i < offers.length; i++) {
-      offers1[i].addEventListener('click', this.#offerToggleHandler);
-    }
-
-
     this.element.querySelector('.card__date-deadline-toggle')
       .addEventListener('click', this.#dueDateToggleHandler);
     this.element.querySelector('.card__repeat-toggle')
@@ -168,11 +243,6 @@ export class TaskEditView extends SmartView {
 
     this.element.querySelector('.card__date-deadline-toggle')
       .addEventListener('click', this.#dueDateToggleHandler);
-  }
-
-  #offerToggleHandler = (evt) => {
-    evt.target.classList.contains('');
-
   }
 
 
