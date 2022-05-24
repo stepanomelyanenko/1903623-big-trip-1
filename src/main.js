db8bf6583bd26153673bb58096813b8f851ad64c
Stepan Omelyanenko <ose.26@mail.ru>
2022-05-24 07:58:52 +0500

    7.1.9 Добавит презентер фильтров
    - По образу презентера доски создадим презентер фильтров
    - В презентер фильтров передадим модель фильтров и подпишем презентер на ее изменения
    - Научим презентер считать количество задач и формировать данные для представления (геттер filters)
    - Используем презентер в точке входа

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import {render, RenderPosition} from './utils/render.js';
 import TripTabsView from './view/trip-tabs-view.js';
-import TripFiltersView from './view/trip-filters-view.js';
 import TripPresenter from './presenter/trip-presenter';
+import FilterPresenter from './presenter/filter-presenter';
 import {generatePoint} from './mock/point.js';
 import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model';
 
 
 const TRIP_POINTS_COUNT = 10;
-
-const filters = ['everything', 'future', 'past'];
 
 const tripPoints = Array.from({length: TRIP_POINTS_COUNT}, generatePoint);
 const pageMainElement = document.querySelector('.page-body');
@@ -23,8 +21,11 @@ pointsModel.points = tripPoints;
 const filterModel = new FilterModel();
 
 render(tripControlsNavigationElement, new TripTabsView(), RenderPosition.BEFOREEND);
-render(tripControlsFiltersElement, new TripFiltersView(filters, 'everything'), RenderPosition.BEFOREEND);
 
 const tripPresenter = new TripPresenter(pageMainElement, pointsModel);
+const filterPresenter = new FilterPresenter(tripControlsFiltersElement, filterModel);
+
+filterPresenter.init();
 tripPresenter.init();
+
 
