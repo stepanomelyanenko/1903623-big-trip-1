15d5528b275eff40d0c7ef49adbaf3d7c37b950a
Stepan Omelyanenko <ose.26@mail.ru>
2022-06-03 10:58:01 +0500

    9.2.2 Поправил один заголовок

--- src/view/point-add-view.js
+++ src/view/point-add-view.js
@@ -81,7 +81,7 @@ const createPointAddTemplate = (point, destinations, allOffers) => {
                 <section class="event__details ${isDisabled ? 'visually-hidden' : ''}">
                        ${editedOffersMarkup}
                     <section class="event__section  event__section--destination">
-                    <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+                    ${destination.description ? '<h3 class="event__section-title  event__section-title--destination">Destination</h3>': ''}
                     <p class="event__destination-description">${destination.description ? destination.description : ''}</p>
                     <div class="event__photos-container">
                       <div class="event__photos-tape">
